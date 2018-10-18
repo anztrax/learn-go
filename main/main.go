841fb1d255c82f121eae076e1bf7afff82fa5221
andrew ananta <kuro.ketanitem@gmail.com>
2018-10-18 21:27:58 +0700

    add how to use go cairo

--- main/main.go
+++ main/main.go
@@ -1,88 +1,40 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/gorilla/mux"
-	"html/template"
-	"log"
+	"github.com/ungerik/go-cairo"
+	"image"
+	_ "image/jpeg"
 	"net/http"
-	"path/filepath"
-	"testGoProject/stringutil"
 )
 
-type Todo struct{
-	Title string
-	Done bool
-}
-
-type TodoPageData struct{
-	PageTitle string
-	Todos []Todo
-}
-
-type User struct{
-	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
-	Age int `json:"age"`
-}
-
-//basic middleware
-func logging(f http.HandlerFunc)  http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
-		log.Println(r.URL.Path)
-		f(w,r)
+func decodeBase64() image.Image{
+	res, err := http.Get("https://tvlk.imgix.net/imageResource/2018/10/15/1539606255527-bc6318215478e9e8a66ba763a7605bea.png?auto=compress%2Cformat&cs=srgb&fm=png&ixlib=java-1.1.12&q=75");
+	if err != nil || res.StatusCode != 200 {
+		// handle errors
+		panic("can't get image")
 	}
+	defer res.Body.Close()
+	m, _, err := image.Decode(res.Body)
+	if err != nil {
+		// handle error
+
+		panic("can't get image")
+	}
+	return m
 }
 
-func main(){
-	// mux router
-	r := mux.NewRouter();
+func createImage(){
+	surface := cairo.NewSurface(cairo.FORMAT_ARGB32, 6000, 6000)
+	surface.SelectFontFace("serif", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+	surface.SetFontSize(32.0)
+	surface.SetSourceRGB(0.0, 0.0, 1.0)
+	surface.MoveTo(10.0, 50.0)
+	surface.ShowText("Hello World")
+	surface.SetImage(decodeBase64());
+	surface.WriteToPNG("hello.png")
+	surface.Finish()
+}
 
-	//html template
-	filePrefix, _ := filepath.Abs("./static/")
-	tmpl := template.Must(template.ParseFiles(filePrefix + "/layout.html"))
-
-	r.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request){
-		var user User
-		json.NewDecoder(r.Body).Decode(&user)
-		fmt.Fprintf(w,"%s %s is %d years old !", user.Firstname, user.Lastname, user.Age)
-	})
-
-	r.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request){
-		peter := User{
-			"John",
-			"Doe",
-			25,
-		}
-
-		json.NewEncoder(w).Encode(peter)
-	})
-
-	r.HandleFunc("/", logging(func(w http.ResponseWriter, r *http.Request){
-		data := TodoPageData{
-			PageTitle:"My Todo List",
-			Todos: []Todo{
-				{Title: "task 1", Done: false},
-				{Title: "Task 2", Done: true},
-				{Title: "Task 3", Done: true},
-			},
-		}
-		tmpl.Execute(w, data)
-	})).Methods("GET")
-
-
-	r.HandleFunc("/books/{title}/page/{page}", logging(func(w http.ResponseWriter, r *http.Request){
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-
-		fmt.Fprintf(w, "You're requested the book : %s on the page %s\n", title, page)
-	})).Methods("GET")
-
-	//fs := http.FileServer(http.Dir("static/"))
-	//http.Handle("/static", http.StripPrefix("/static", fs))
-
-	http.ListenAndServe(":8010", r)
-	fmt.Printf("%s", stringutil.Reverse("hello there"));
+func main() {
+	createImage()
 }
---
+++ main/main3.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"testGoProject/stringutil"
+)
+
+type Todo struct{
+	Title string
+	Done bool
+}
+
+type TodoPageData struct{
+	PageTitle string
+	Todos []Todo
+}
+
+type User struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+	Age int `json:"age"`
+}
+
+//basic middleware
+func logging(f http.HandlerFunc)  http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		log.Println(r.URL.Path)
+		f(w,r)
+	}
+}
+
+func main3(){
+	// mux router
+	r := mux.NewRouter();
+
+	//html template
+	filePrefix, _ := filepath.Abs("./static/")
+	tmpl := template.Must(template.ParseFiles(filePrefix + "/layout.html"))
+
+	r.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request){
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+		fmt.Fprintf(w,"%s %s is %d years old !", user.Firstname, user.Lastname, user.Age)
+	})
+
+	r.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request){
+		peter := User{
+			"John",
+			"Doe",
+			25,
+		}
+
+		json.NewEncoder(w).Encode(peter)
+	})
+
+	r.HandleFunc("/", logging(func(w http.ResponseWriter, r *http.Request){
+		data := TodoPageData{
+			PageTitle:"My Todo List",
+			Todos: []Todo{
+				{Title: "task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})).Methods("GET")
+
+
+	r.HandleFunc("/books/{title}/page/{page}", logging(func(w http.ResponseWriter, r *http.Request){
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You're requested the book : %s on the page %s\n", title, page)
+	})).Methods("GET")
+
+	//fs := http.FileServer(http.Dir("static/"))
+	//http.Handle("/static", http.StripPrefix("/static", fs))
+
+	http.ListenAndServe(":8010", r)
+	fmt.Printf("%s", stringutil.Reverse("hello there"));
+}
